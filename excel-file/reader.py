bb51c4b59c0db79aceb063c57408f5f23a630466
Diogo Faria <diogofaria073@gmail.com>
2023-08-18 06:06:01 -0300

    More exercises to practice

--- excel-file/reader.py
+++ excel-file/reader.py
@@ -1,5 +1,6 @@
 import os as os
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # reader excel file from os directory
 path = os.getcwd()
@@ -16,5 +17,11 @@ if files_founded:
             file, sheet_name='Sheet1').dropna(axis=1, how='all').dropna(axis=0, how='all').dropna(
             axis=0, subset=['Item', 'Valor']).reset_index(drop=True).sort_values(
             by=['Valor'], ascending=False).reset_index(drop=True)
+
+      # print a bar chart with 2 dimensions. First dimension is the Item column and the second dimension is the Valor column.
+        plt.pie(excel_data['Valor'], labels=excel_data['Item'],
+                autopct='%1.1f%%', startangle=90)
+        plt.show()
+
 else:
     print('File not found')
